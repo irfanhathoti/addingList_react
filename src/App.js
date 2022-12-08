07cebd36727ff05855729feca3898506484bfbee
irfan.hathoti <irfan.hathoti@htcinc.com>
2022-12-08 22:33:56 +0530

    'first'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import StudentList from './Components/Crud/StudentList';
+// import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+// import DashBoard from './Components/DashBoard';
+// import AddBook from './Components/AddBook';
+// import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StudentList />
+       {/* <Router>
+         <Header />
+        <Switch>
+          <Route path="/" exact component={
+            () => {
+              return (<DashBoard />)
+            }} />
+            <Route path="/ShowData" exact component={
+            () => {
+              return (<AddBook />)
+            }} />
+        </Switch>
+
+      </Router> */}
     </div>
   );
 }
